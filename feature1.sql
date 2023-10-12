83466b3320c94671c7c4f2b69de716e27647c1b3
Ciara Ni Shiochru <ciarasugrue@gmail.com>
2023-10-12 13:56:51 +0100

    removing select all statement

--- feature1.sql
+++ feature1.sql
@@ -10,27 +10,18 @@ CREATE TABLE Team (
 CREATE TABLE Employee (
 	employee_id MEDIUMINT PRIMARY KEY AUTO_INCREMENT,
     employee_name VARCHAR(255),
-    salary MEDIUMINT,
+    salary DECIMAL(10,2),
     bank_account_number CHAR(8),
     national_insurance_number CHAR(9),
-    team_id MEDIUMINT
-	);
+    team_id MEDIUMINT,
+    FOREIGN KEY (team_id) REFERENCES Team(team_id)
+	);   
     
 CREATE TABLE SalesEmployee (
-	sales_employee_id MEDIUMINT PRIMARY KEY AUTO_INCREMENT,
-    employee_id MEDIUMINT,
-    commission_rate DECIMAL(10, 2)
+	employee_id MEDIUMINT PRIMARY KEY,
+    commission_rate DECIMAL(10, 2),
+    FOREIGN KEY (employee_id) REFERENCES Employee(employee_id)
     );
-    
-ALTER TABLE Employee
-ADD CONSTRAINT fk_team_id
-FOREIGN KEY (team_id)
-REFERENCES Team(team_id);
-
-ALTER TABLE SalesEmployee
-ADD CONSTRAINT fk_employee_id
-FOREIGN KEY (employee_id)
-REFERENCES Employee(employee_id);
 
 -- Insert fake data into the Team table
 INSERT INTO Team (team_type)
@@ -45,8 +36,6 @@ VALUES
     ('Jane Smith', 55000, '23456789', 'CD234567D', 2),
     ('David Johnson', 70000, '34567890', 'EF345678E', 2),
     ('Emily Davis', 48000, '45678901', 'GH456789F', 1);
-    
-select * from Employee;
 
 -- Insert fake data into the SalesEmployee table
 INSERT INTO SalesEmployee (employee_id, commission_rate)
@@ -56,5 +45,7 @@ VALUES
 
 -- We are assuming that we can restrict to HR employees to add a Sales employee using Java/web dev technologies.
 -- Query to find HR employees:
-SELECT * FROM Employee
-WHERE team_id = 2;
+SELECT employee_name AS `Name`, salary AS `Salary`,bank_account_number AS `Bank Account Number`, 
+national_insurance_number AS `National Insurance Number`, commission_rate AS `Commission Rate` 
+FROM SalesEmployee SE
+LEFT JOIN Employee E ON E.employee_id = SE.employee_id;
